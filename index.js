ef71c2c29de01619e7724869fa78eb35def2c5c3
michelle nderi <nderimichelle@gmail.com>
2023-03-31 21:11:33 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let viewed = new Set();
+
+  for (let i = 0; i < array.length; i++) {
+    let num = target - array[i];
+
+viewed.add(array[i]);
+
+if(viewed.has(num)){
+  return true;
+}
+  }
+return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
@@ -11,12 +22,18 @@ function hasTargetSum(array, target) {
 */
 
 /*
-  Add written explanation of your solution here
+  Create a new set that store the already viewed numbers in the array.
+  Iterate through the array to find the num.
+  Check if we have already viewed the array and return to true if we have.
+  Add the current num to the set .
+
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 9, 7, 5], 17));
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
